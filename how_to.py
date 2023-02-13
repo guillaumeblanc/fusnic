859a0d8793bb4fefd4b20daf5ce8526bac8b36bc
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2023-02-13 14:14:13 +0100

    Refactors to be executed as a script.

--- how_to.py
+++ how_to.py
@@ -4,28 +4,35 @@ import logging
 from datetime import datetime, timezone
 
 
-user = os.environ.get('FUSIONSOLAR_USER', 'unknown_user')
-password = os.environ.get('FUSIONSOLAR_PASSWORD', 'unknown_password')
+def how_to(user: str, password: str):
+    '''
+    Demonstrates how to log in FusionSolar, query plants list and hourly data.
+    '''
 
-'''
-Demonstrates how to log in FusionSolar and query plants list.
-'''
+    try:
+        with fusnic.ClientSession(user=user, password=password) as client:
+            plants = client.get_plant_list()
+            print('Plants list:\n' + str(plants))
 
-try:
-    with fusnic.ClientSession(user=user, password=password) as client:
-        plants = client.get_plant_list()
-        print('Plants list:\n' + str(plants))
+            # Extract list of plant codes
+            plants_code = [plant['stationCode'] for plant in plants]
 
-        # Extract list of plant codes
-        plants_code = [plant['stationCode'] for plant in plants]
+            # Query latest hourly data for all plants
+            hourly = client.get_plant_hourly_data(
+                plants_code, datetime.now(timezone.utc))
+            print('Hourly KPIs:\n' + str(hourly))
 
-        # Query latest hourly data for all plants
-        hourly = client.get_plant_hourly_data(
-            plants_code, datetime.now(timezone.utc))
-        print('Hourly KPIs:\n' + str(hourly))
+    except fusnic.LoginFailed:
+        logging.error(
+            'Login failed. Verify user and password of Northbound API account.')
+    except fusnic.FrequencyLimit:
+        logging.error('FusionSolar interface access frequency is too high.')
+    except fusnic.Permission:
+        logging.error(
+            'Missing permission to access FusionSolar Northbound interface.')
 
-except fusnic.LoginFailed:
-    logging.error(
-        'Login failed. Verify user and password of Northbound API account.')
-except fusnic.FrequencyLimit:
-    logging.error('FusionSolar interface access frequency is too high.')
+
+if __name__ == '__main__':
+    user = os.environ.get('FUSIONSOLAR_USER', 'unknown_user')
+    password = os.environ.get('FUSIONSOLAR_PASSWORD', 'unknown_password')
+    how_to(user=user, password=password)
